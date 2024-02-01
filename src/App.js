b12fce6b0ca98d07effdf2878cd28bb45677bd65
Eric Laptop <erwong25@gmail.com>
2024-01-31 16:17:58 -0800

    Changes made on laptop

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import * as React from "react";
 
 function App(): React.Node {
   const [diceSize, setDiceSize] = useState("");
+  const [joinDice, setJoinDice] = useState([]);
   const [error, setError] = useState(null);
   const [status, setStatus] = useState("typing");
   const [result, setResult] = useState(new Map<number, Array<number>>());
@@ -24,34 +25,63 @@ function App(): React.Node {
   function handleSubmit(e: Event) {
     e.preventDefault();
     setStatus("submitting");
-    if (diceSize.includes("d")) {
-      setResult(
-        new Map([
-          ["5", [3]],
-          ["8", [4, 5]],
-        ])
-      );
-      setStatus("typing");
-    } else {
-      let diceSizeInt = parseInt(diceSize);
-      let shouldError = diceSizeInt <= 0 || isNaN(diceSizeInt);
-      try {
-        if (shouldError) {
-          throw new Error("Not a valid number");
-        } else {
-          let resultUpdate = result;
-          result.has(diceSizeInt)
-            ? resultUpdate.get(diceSizeInt).push(roll(diceSize))
-            : result.set(diceSizeInt, [roll(diceSize)]);
-          setResult(new Map(resultUpdate));
-        }
-        setStatus("typing");
-      } catch (err) {
-        setStatus("typing");
-        setError(err);
+    let parseDice = diceSize.split("d");
+    let diceSizeInt = parseInt(diceSize);
+    let shouldError = !parseDice.some((i) => !Number.isInteger(i));
+    //setParseDice is async or parseDice is async so its not updating before the rest of the code goes through
+    //need to call the split somewhere else probably
+    try {
+      if (shouldError) {
+        throw new Error("Not a valid number");
+      } else if (result.has(diceSizeInt)) {
+        let resultUpdate = result;
+        resultUpdate.get(diceSizeInt).push(roll(diceSizeInt));
+        setResult(new Map(resultUpdate));
+        setJoinDice(parseDice);
+      } else {
+        let resultUpdate = result;
+        result.set(diceSizeInt, [roll(diceSizeInt)]);
+        setResult(new Map(resultUpdate));
+        setJoinDice(parseDice);
       }
+      setStatus("typing");
+    } catch (err) {
+      setStatus("typing");
+      setError(err);
     }
   }
+
+  // function handleSubmit(e: Event) {
+  //   e.preventDefault();
+  //   setStatus("submitting");
+  //   if (diceSize.includes("d")) {
+  //     setResult(
+  //       new Map([
+  //         ["5", [3]],
+  //         ["8", [4, 5]],
+  //       ])
+  //     );
+  //     setStatus("typing");
+  //   } else {
+  //     let diceSizeInt = parseInt(diceSize);
+  //     let shouldError = diceSizeInt <= 0 || isNaN(diceSizeInt);
+  //     try {
+  //       if (shouldError) {
+  //         throw new Error("Not a valid number");
+  //       } else {
+  // let resultUpdate = result;
+  // result.has(diceSizeInt)
+  //   ? resultUpdate.get(diceSizeInt).push(roll(diceSize))
+  //   : result.set(diceSizeInt, [roll(diceSize)]);
+  // setResult(new Map(resultUpdate));
+  //       }
+  //       setStatus("typing");
+  //     } catch (err) {
+  //       setStatus("typing");
+  //       setError(err);
+  //     }
+  //   }
+  // }
 
   function handleTextareaChange(e: SyntheticInputEvent<HTMLInputElement>) {
     setDiceSize(e.target.value);
@@ -79,6 +109,7 @@ function App(): React.Node {
             </button>
             {error !== null && <p className="Error">{error.message}</p>}
             {DiceResultsSection(result)}
+            {joinDice.join(", ")}
           </form>
         </header>
       </div>
--- src/DiceResultsSection.js
+++ src/DiceResultsSection.js
@@ -6,6 +6,7 @@ export default function DiceResultsSection(result) {
           {[...result.keys()].map((k) => (
             <li key={k}>
               You rolled {result.get(k).length}d{k} and got{" "}
+              {/* uhh what why is that {" "} at the end? when i delete it and save the auto formatting adds it backa*/}
               {result.get(k).join(", ")}
             </li>
           ))}
